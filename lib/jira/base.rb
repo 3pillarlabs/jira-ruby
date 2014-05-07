3b4ba59160dcf3a69e56f22bc5fc248a3a7337ee
SideofClouds <dan_fodorean_cj@yahoo.com>
2014-05-07 17:05:13 +0300

    added :jql parameter to query_params_for_search in order to make custom queries in Product issues method

--- lib/jira/base.rb
+++ lib/jira/base.rb
@@ -52,7 +52,7 @@ module JIRA
   #
   class Base
     QUERY_PARAMS_FOR_SINGLE_FETCH = Set.new [:expand, :fields]
-    QUERY_PARAMS_FOR_SEARCH = Set.new [:expand, :fields, :startAt, :maxResults]
+    QUERY_PARAMS_FOR_SEARCH = Set.new [:jql, :expand, :fields, :startAt, :maxResults]
 
     # A reference to the JIRA::Client used to initialize this resource.
     attr_reader :client
